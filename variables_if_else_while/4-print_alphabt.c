13349e5f3f9ffc9b7cdf12aa9db472804c8f941b
root <dienebayeabdou90@gmail.com>
2025-02-20 03:05:32 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,11 @@ int main(void)
 {char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
-{ if (letter != 'q' && letter != 'e')
-putchar(letter); }
+    {
+if (letter != 'q' && letter != 'e')
+{putchar(letter);}
+    }
 
 putchar('\n');
-return(0);}
+
+return (0); }
