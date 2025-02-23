a6b8d14f352960f944bbeb7ea5a21b663aa8bc48
root <dienebayeabdou90@gmail.com>
2025-02-23 15:55:51 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,10 @@ void print_alphabet_x10(void)
 {int i, ch;
 
 for (i = 0; i < 10; i++)
-{for (ch = 'a'; ch <= 'z'; ch++)
-{_putchar(ch); }
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
 _putchar('\n'); }
 }
