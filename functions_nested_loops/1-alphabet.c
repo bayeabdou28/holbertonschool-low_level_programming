c6062ac58960f70c9af17da549f352c1dda1c83f
root <dienebayeabdou90@gmail.com>
2025-02-23 15:21:31 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,7 +6,11 @@
  * Return: void
  */
 void print_alphabet(void)
-{char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar('\n'); }
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+        _putchar(letter);
+
+    _putchar('\n');
+}
