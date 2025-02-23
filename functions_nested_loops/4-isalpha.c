1fa0c31bc42696b5fa066bfca7b86c7e21457fdf
root <dienebayeabdou90@gmail.com>
2025-02-23 18:38:10 +0000

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,2 +1,8 @@
+/**
+ * _isalpha - Vérifie si un caractère est alphabétique
+ * @c: Le caractère à vérifier
+ *
+ * Return: 1 si c est une lettre (minuscule ou majuscule), 0 sinon
+ */
 int _isalpha(int c)
 {return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')); }
