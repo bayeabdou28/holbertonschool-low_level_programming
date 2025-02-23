48c520e647596ac9ca2fb05fa2bb092298ff5266
root <dienebayeabdou90@gmail.com>
2025-02-23 16:27:40 +0000

    3-islower.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,7 @@
-/**
- * _islower - Vérifie si un caractère est en minuscule
- * @c: Le caractère à vérifier
- *
- * Return: 1 si c est une lettre minuscule, 0 sinon
- */
-int _islower(int c)
-{
-    if (c >= 'a' && c <= 'z')
-        return (1);
-    else
-        return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _islower(int c);
+
+#endif /* MAIN_H */
