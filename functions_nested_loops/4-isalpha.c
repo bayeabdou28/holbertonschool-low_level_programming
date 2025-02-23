c7db32885a6defd781e20d23f8d86bfa127be239
root <dienebayeabdou90@gmail.com>
2025-02-23 16:46:49 +0000

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,2 +1,8 @@
+/**
+ * _islower - Vérifie si un caractère est en minuscule
+ * @c: Le caractère à vérifier
+ *
+ * Return: 1 si c est une lettre minuscule, 0 sinon
+i */
 int _isalpha(int c)
 {return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')); }
