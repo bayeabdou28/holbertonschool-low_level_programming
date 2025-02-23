c459a260731c977b2e1c30080e15434c8f20a467
root <dienebayeabdou90@gmail.com>
2025-02-23 18:51:09 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,23 +1,8 @@
-#include "main.h"
-
 /**
- * print_sign - Imprime le signe d'un nombre
- * @n: Le nombre à vérifier
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
  *
- * Return: 1 si n est positif, 0 si n est zéro, -1 si n est négatif
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
-int print_sign(int n)
-{
-if (n > 0)
-{
-_putchar('+');
-return (1); }
-else if (n == 0)
-{
-_putchar('0');
-return (0); }
-else
-{
-_putchar('-');
-return (-1); }
-}
+int _isalpha(int c)
+{return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));\ }
