75a68ad07b431efd59b79a15ca8d2cde8d85a591
root <dienebayeabdou90@gmail.com>
2025-02-25 12:09:22 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,8 @@
  */
 void print_numbers(void)
 {
-_putchar('0');  /* Affiche 0 */
-_putchar('1');  /* Affiche 1 */
-_putchar('2');  /* Affiche 2 */
-_putchar('3');  /* Affiche 3 */
-_putchar('4');  /* Affiche 4 */
-_putchar('5');  /* Affiche 5 */
-_putchar('6');  /* Affiche 6 */
-_putchar('7');  /* Affiche 7 */
-_putchar('8');  /* Affiche 8 */
-_putchar('9');  /* Affiche 9 */
-_putchar('\n'); /* Affiche un saut de ligne */ }
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
+_putchar(digit);
+_putchar('\n');
+}
