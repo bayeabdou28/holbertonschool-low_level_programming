ef24046d1e4dbcd4d5d5b2abef1d04d41a26bed5
root <dienebayeabdou90@gmail.com>
2025-02-25 10:16:10 +0000

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,9 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-    char c1 = 'A';
-    char c2 = 'z';
-
-    printf("%c: %d\n", c1, _isupper(c1)); // Doit afficher A: 1
-    printf("%c: %d\n", c2, _isupper(c2)); // Doit afficher z: 0
-
-    return (0);
-}
-
+char c1 = 'A';
+printf("%c: %d\n", c1, _isupper(c1)); /* Doit afficher A: 1 */
+/* ... reste du code ... */
+return (0); }
