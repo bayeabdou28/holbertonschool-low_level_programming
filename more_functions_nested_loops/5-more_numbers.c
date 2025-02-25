f22c6e3b8d0a9d008867169ca93ebf09885d7c33
root <dienebayeabdou90@gmail.com>
2025-02-25 12:33:30 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,8 +4,7 @@
  * more_numbers - prints 10 times the numbers, from 0 to 14
  */
 void more_numbers(void)
-{
-int i, j;
+{int i, j;
     
 for (i = 0; i < 10; i++)
 {
@@ -13,8 +12,6 @@ for (j = 0; j <= 14; j++)
 {
 if (j > 9)
 _putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-}
-_putchar('\n');
-}
+_putchar((j % 10) + '0'); }
+_putchar('\n'); }
 }
