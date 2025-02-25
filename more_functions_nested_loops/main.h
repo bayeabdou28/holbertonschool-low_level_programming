242a0a47256d84e18abf2cc1c10df5253223a8ad
root <dienebayeabdou90@gmail.com>
2025-02-25 13:12:56 +0000

    10-print_triangle.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@ void print_numbers(void);
 void print_diagonal(int n);
 void print_square(int size);
 void print_line(int n);
+void print_triangle(int size);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
