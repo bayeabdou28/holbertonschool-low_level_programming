5a71c680780a35e44b45b13e877433dc9fe2a0a2
root <dienebayeabdou90@gmail.com>
2025-02-25 12:59:59 +0000

    8-print_square.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ void more_numbers(void);
 int mul(int a, int b);
 void print_numbers(void);
 void print_diagonal(int n);
+void print_square(int size);
 void print_line(int n);
 int _putchar(char c);
 int _isupper(int c);
