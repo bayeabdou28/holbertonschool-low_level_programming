2c12f0fd90e7622ab883c1a1a1e5a34d8cbdc510
root <dienebayeabdou90@gmail.com>
2025-02-28 13:49:14 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 int _strlen(char *s)
 {
-    int length = 0; /* Initialiser le compteur à 0 */
+int length = 0; /* Initialiser le compteur à 0 */
 
-    /* Parcourir chaque caractère jusqu'à ce qu'on atteigne '\0' */
-    while (s[length] != '\0')
-    {
-        length++; /* Incrémenter le compteur pour chaque caractère */
-    }
+/* Parcourir chaque caractère jusqu'à ce qu'on atteigne '\0' */
+while (s[length] != '\0')
+{
+length++; /* Incrémenter le compteur pour chaque caractère */
+}
 
-    return length; /* Retourner la longueur de la chaîne */
+return length; /* Retourner la longueur de la chaîne */
 }
