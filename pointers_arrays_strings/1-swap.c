83f0fa8450155db29939c9b487f840d5920f8be7
root <dienebayeabdou90@gmail.com>
2025-02-28 11:32:35 +0000

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,19 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
-void swap_int(int *a, int *b);
-
-int main() {
-    int x = 10, y = 20;
-    printf("Avant échange : x = %d, y = %d\n", x, y);
-
-    swap_int(&x, &y);  /* On passe les adresses de x et y */
-    printf("Après échange : x = %d, y = %d\n", x, y);
-
-    return 0;
-}
-
+/* Fonction qui échange les valeurs de deux entiers */
 void swap_int(int *a, int *b) {
-    int temp = *a;  /* Stocke la valeur de *a */
-    *a = *b;        /* Met *b dans *a */
-    *b = temp;      /* Met temp (ancienne *a) dans *b */
+int temp = *a;
+*a = *b;
+*b = temp;
 }
