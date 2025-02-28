5d277358dad9e3f9ad6d0bd53f6e6b74c1b2c971
root <dienebayeabdou90@gmail.com>
2025-02-28 11:38:32 +0000

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -11,8 +11,8 @@
 #include "main.h"
 
 /* Fonction qui échange les valeurs de deux entiers */
-void swap_int(int *a, int *b) {
-int temp = *a;
+void swap_int(int *a, int *b) 
+{int temp = *a;
 *a = *b;
 *b = temp;
 }
