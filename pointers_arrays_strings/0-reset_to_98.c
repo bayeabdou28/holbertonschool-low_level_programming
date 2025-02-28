dc79c99b9a68d0604782648c173eaa5ef510897b
root <dienebayeabdou90@gmail.com>
2025-02-28 10:11:22 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 
-// Fonction qui met la valeur de n à 98
-void reset_to_98(int *n) {
-    *n = 98;
+void reset_to_98(int *n)
+{
+*n = 98;
 }
 
-int main() {
-int var = 42;  // Déclaration d'un entier
-printf("Avant : %d\n", var);
-
-reset_to_98(&var);  // On passe l'adresse de var à la fonction
-printf("Après : %d\n", var);  // La valeur de var est maintenant 98
-
-return 0; }
