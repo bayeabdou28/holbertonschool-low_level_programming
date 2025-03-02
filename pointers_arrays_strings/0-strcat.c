9bf5ef550fc065c78d3413ebc7528c615224a625
‘bayeabdou28 <dienebayeabdou90@gmail.com>
2025-03-02 20:25:07 +0000

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to be appended
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+char *original_dest = dest;
+
+/* Move to the end of dest string */
+while (*dest != '\0')
+dest++;
+
+/* Copy characters from src to dest */
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+
+/* Add terminating null byte */
+*dest = '\0';
+
+return original_dest; }
