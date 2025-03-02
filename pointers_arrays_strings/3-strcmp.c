99e0129cdbf9eaae8c693ff94cd66d146dc7aebe
‘bayeabdou28 <dienebayeabdou90@gmail.com>
2025-03-02 20:49:12 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: 0 if strings are equal, negative if s1 < s2, positive if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && (*s1 == *s2))
+{
+s1++;
+s2++;
+}
+return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
