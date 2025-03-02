b3532956da6b40bb58c08e7c0abde9463141951a
‘bayeabdou28 <dienebayeabdou90@gmail.com>
2025-03-02 20:46:14 +0000

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,11 @@
+/**
+ * _strncpy - Copies a string
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The number of bytes to copy
+ *
+ * Return: A pointer to the resulting string dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
