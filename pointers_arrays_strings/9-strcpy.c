d70be044f8ec6d6376508dc5c69ef71918182d91
‘bayeabdou28 <dienebayeabdou90@gmail.com>
2025-03-02 19:50:33 +0000

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,10 @@
+/**
+ * _strcpy - Copies the string pointed to by src to dest
+ * @dest: Pointer to the destination array where the content is to be copied
+ * @src: String to be copied
+ *
+ * Return: Pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 char *original_dest = dest;
