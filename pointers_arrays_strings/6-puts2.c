491c5b410d5ee9929762eb4ba97d5ed47083794f
‘bayeabdou28 <dienebayeabdou90@gmail.com>
2025-03-02 19:19:26 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Affiche un caractère sur deux d'une chaîne, en commençant par le premier
+ * @str: Pointeur vers la chaîne de caractères
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0') // Parcours de la chaîne
+{
+_putchar(str[i]);  // Affiche le caractère actuel
+i += 2;            // Passe au caractère suivant en sautant un
+}
+_putchar('\n');        // Ajout du saut de ligne
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts(char *str);
+void puts2(char *str);
 int _putchar(char c);
-void rev_string(char *s);
 
 #endif /* MAIN_H */
