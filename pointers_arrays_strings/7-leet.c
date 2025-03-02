f13483ecdcea6cbd37891568d19d901e989834c5
‘bayeabdou28 <dienebayeabdou90@gmail.com>
2025-03-02 21:07:18 +0000

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - Encodes a string into 1337
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ */
+char *leet(char *str)
+{
+int i, j;
+char leet_letters[] = "aAeEoOtTlL";
+char leet_numbers[] = "4433007711";
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; leet_letters[j] != '\0'; j++)
+{
+if (str[i] == leet_letters[j])
+{
+str[i] = leet_numbers[j];
+break;
+}
+}
+}
+
+return (str;)
+}
