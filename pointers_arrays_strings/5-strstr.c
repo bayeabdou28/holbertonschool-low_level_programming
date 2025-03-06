b7583f0ef07b282d0cbf8296c210c73cdbe249da
â <dienebayeabdou90@gmail.com>
2025-03-06 23:52:18 +0000

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,36 @@
+#include "main.h"
+
 /**
- * _strstr - Locate a substring in a string
- * @haystack: The main string to search in
- * @needle: The substring to search for
+ * _strstr - Locates a substring.
+ * @haystack: The main string to search in.
+ * @needle: The substring to find.
  *
- * Return: Pointer to the beginning of the located substring,
- * or NULL if the substring is not found
+ * Return: Pointer to the beginning of the located substring, or NULL if not found.
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+char *h, *n;
 
-    if (*needle == '\0') /* Si le substring est vide, retourne haystack */
-        return (haystack);
+if (*needle == '\0') /* Si needle est vide, retourne haystack */
+return (haystack);
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j]) /* Vérifie si les caractères correspondent */
-                break;
-        }
-        if (needle[j] == '\0') /* Si on a parcouru tout needle, retourne le pointeur */
-            return (haystack + i);
-    }
-    return (NULL); /* Retourne NULL si le substring n'est pas trouvé */
+while (*haystack)
+{
+h = haystack;
+n = needle;
+
+while (*h && *n && (*h == *n)) /* Vérifie si les caractères correspondent */
+{
+h++;
+n++;
 }
 
+if (*n == '\0') /* Si on a atteint la fin de needle, on a trouvé la sous-chaîne */
+return (haystack);
+
+haystack++;
+}
+
+return (NULL); /* Si la sous-chaîne n'est pas trouvée */
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
-
