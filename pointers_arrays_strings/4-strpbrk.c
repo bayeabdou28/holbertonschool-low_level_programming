fb9fcc55f93fcdc636c6fb0ce036ec0e4a9f746b
â <dienebayeabdou90@gmail.com>
2025-03-06 23:21:37 +0000

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,25 @@
 /**
- * _strpbrk - Searches a string for any of a set of bytes
- * @s: The string to be searched
- * @accept: The string containing the bytes to look for
- *
- * Return: Pointer to the byte in s that matches one of the bytes in accept,
- *         or NULL if no such byte is found
- */
+
+_strpbrk - Recherche un ensemble de bytes dans une chaîne
+
+@s: Chaîne à analyser
+
+@accept: Ensemble de bytes à rechercher
+
+Return: Pointeur vers le byte trouvé, ou NULL si non trouvé
+*/
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
-
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-                return (s + i);
-        }
-    }
-
-    return (NULL);
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (s + i);
+}
+}
+}
+return (NULL);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 
 #endif /* MAIN_H */
 
