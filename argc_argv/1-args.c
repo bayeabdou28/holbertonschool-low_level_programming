1bb52b67b8c3997bec81d5130526f69a980a5ee0
root <dienebayeabdou90@gmail.com>
2025-03-07 17:38:22 +0000

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-    /* On affiche simplement argc, qui contient le nombre d'arguments */
-    printf("%d\n", argc - 1); /* Soustraire 1 pour ne pas compter le programme lui-même */
-    return 0;
+/* On affiche simplement argc, qui contient le nombre d'arguments */
+printf("%d\n", argc - 1); /* Soustraire 1 pour ne pas compter le programme lui-même */
+return 0;
 }
