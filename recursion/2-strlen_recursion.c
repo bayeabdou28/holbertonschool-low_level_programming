0a097644b6577aefedc3fb1a0c1b6d1482a5d2e1
root <dienebayeabdou90@gmail.com>
2025-03-07 15:10:58 +0000

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: Length of the string (int)
+ */
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0') /* Base case: if we reach the end of the string */
+    {
+        return (0);
+    }
+
+    return (1 + _strlen_recursion(s + 1)); /* Add 1 and call function recursively */
+}
+
