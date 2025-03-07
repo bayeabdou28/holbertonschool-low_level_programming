ca4697337c58c3964b0262ca945927bb59eb0fc7
root <dienebayeabdou90@gmail.com>
2025-03-07 16:00:45 +0000

    100-is_palindrome.c

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -8,9 +8,9 @@
  */
 int _strlen(char *s)
 {
-if (*s == '\0') /* Si la chaîne est vide */
-return (0);
-return (1 + _strlen(s + 1)); /* Appel récursif pour la longueur */
+    if (*s == '\0') /* Si la chaîne est vide */
+        return (0);
+    return (1 + _strlen(s + 1)); /* Appel récursif pour la longueur */
 }
 
 /**
@@ -23,13 +23,13 @@ return (1 + _strlen(s + 1)); /* Appel récursif pour la longueur */
  */
 int check_palindrome(char *s, int start, int end)
 {
-if (start >= end) /* Si on atteint le milieu de la chaîne */
-return (1);
+    if (start >= end) /* Si on atteint le milieu de la chaîne */
+        return (1);
 
-if (s[start] != s[end]) /* Si les caractères ne correspondent pas */
-return (0);
+    if (s[start] != s[end]) /* Si les caractères ne correspondent pas */
+        return (0);
 
-return (check_palindrome(s, start + 1, end - 1)); /* Vérifie les caractères suivants */
+    return (check_palindrome(s, start + 1, end - 1)); /* Vérifie les caractères suivants */
 }
 
 /**
@@ -40,8 +40,9 @@ return (check_palindrome(s, start + 1, end - 1)); /* Vérifie les caractères su
  */
 int is_palindrome(char *s)
 {
-int length = _strlen(s); /* Calcul de la longueur de la chaîne */
-if (length == 0) /* Si la chaîne est vide, c'est un palindrome */
-return (1);
-return (check_palindrome(s, 0, length - 1)); /* Appel récursif pour vérifier les caractères */
+    int length = _strlen(s); /* Calcul de la longueur de la chaîne */
+    if (length == 0) /* Si la chaîne est vide, c'est un palindrome */
+        return (1);
+    return (check_palindrome(s, 0, length - 1)); /* Appel récursif pour vérifier les caractères */
 }
+
