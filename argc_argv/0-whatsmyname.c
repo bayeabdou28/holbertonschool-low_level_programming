1ee07b88b74aa153480fb3c0c29f21f1ca1256fb
root <dienebayeabdou90@gmail.com>
2025-03-07 17:27:39 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    /* Imprime le nom du programme avec le chemin complet */
+    (void)argc;
     printf("%s\n", argv[0]);
-    return 0;
+    return (0);
 }
