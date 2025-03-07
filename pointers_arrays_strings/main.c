85628550b99d01c58d00100c357855280a4eaf83
â <dienebayeabdou90@gmail.com>
2025-03-07 00:31:25 +0000

    100-set_string.c

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,22 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * main - Entry point to test _strstr function.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-char *s = "hello, world";
-char *f = "world";
-char *t;
+    char *s0 = "Bob Dylan";
+    char *s1 = "Robert Allen";
 
-t = _strstr(s, f);
-
-if (t)
-_puts(t); /* Utilise _puts pour afficher la sous-chaîne trouvée */
-else
-_puts("Not found");
-
-return (0);
+    printf("%s, %s\n", s0, s1);
+    set_string(&s1, s0);
+    printf("%s, %s\n", s0, s1);
+    return (0);
 }
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 char *_strstr(char *haystack, char *needle);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
