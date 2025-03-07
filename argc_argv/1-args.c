2db286d7d54d9b7f4ce6b98387b60177d3954dfe
root <dienebayeabdou90@gmail.com>
2025-03-07 17:37:07 +0000

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,14 +3,13 @@
 /**
  * main - Prints the number of arguments passed into it
  * @argc: Number of arguments
- * @argv: Array of strings (arguments)
+ * @argv: Array of strings (arguments) (non utilisé ici)
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
     /* On affiche simplement argc, qui contient le nombre d'arguments */
     printf("%d\n", argc - 1); /* Soustraire 1 pour ne pas compter le programme lui-même */
     return 0;
 }
-
