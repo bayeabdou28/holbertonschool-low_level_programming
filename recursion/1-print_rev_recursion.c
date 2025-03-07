30080521f639b270b548e38017d9282d7d82ff47
root <dienebayeabdou90@gmail.com>
2025-03-07 15:07:51 +0000

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,12 +8,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0') /* Base case: end of string */
-    {
-        return;
-    }
-
-    _print_rev_recursion(s + 1); /* Recursive call with next character */
-    _putchar(*s); /* Print the character after the recursion */
+if (*s == '\0') /* Base case: end of string */
+{
+return;
 }
 
+_print_rev_recursion(s + 1); /* Recursive call with next character */
+_putchar(*s); /* Print the character after the recursion */
+}
