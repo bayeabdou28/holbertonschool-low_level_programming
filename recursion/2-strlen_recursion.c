c38886281131f19bc74b28b55c6b85ef422578de
root <dienebayeabdou90@gmail.com>
2025-03-07 15:30:05 +0000

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -12,5 +12,5 @@ if (*s == '\0') /* Base case: if we reach the end of the string */
 {
 return (0);
 }
-return (1 + _strlen_recursion(s + 1)); /* Add 1 and call function recursively */
+return (1 + _strlen_recursion(s + 1)); /* Add 1 and call function recursively */:
 }
