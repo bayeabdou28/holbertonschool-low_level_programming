70d90e3063a9694ef29c1e693721a9a53388d1c2
root <dienebayeabdou90@gmail.com>
2025-03-07 17:51:01 +0000

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,23 +10,22 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
+int num1, num2, result;
 
-    /* Vérifie que deux arguments sont passés */
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-
-    /* Convertit les arguments en entiers et effectue la multiplication */
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-    result = num1 * num2;
-
-    /* Affiche le résultat */
-    printf("%d\n", result);
-
-    return (0);
+/* Vérifie que deux arguments sont passés */
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
 }
 
+/* Convertit les arguments en entiers et effectue la multiplication */
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+
+/* Affiche le résultat */
+printf("%d\n", result);
+
+return (0);
+}
