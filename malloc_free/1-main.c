d5b6905647096ec55468a0907481d335425f88e5
root <dienebayeabdou90@gmail.com>
2025-03-14 16:20:41 +0000

    1-strdup.c

---
+++ malloc_free/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str);
+
+int main(void)
+{
+	char *s;
+
+	s = _strdup("Holberton");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
+
--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,33 +1,25 @@
-#include <stdlib.h> /* Pour malloc et free */
-#include <string.h> /* Pour strlen */
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * _strdup - Crée une copie d'une chaîne de caractères donnée.
- * @str: La chaîne de caractères à copier.
- *
- * Return: Un pointeur vers la nouvelle chaîne copiée, ou NULL si échec.
- */
+* _strdup - Duplique une chaîne dans une nouvelle zone mémoire
+* @str: Chaîne à copier
+*
+* Return: Pointeur vers la nouvelle chaîne, NULL si échec
+*/
 char *_strdup(char *str)
 {
-    char *copy;
-    unsigned int len, i;
+char *duplicate;
+unsigned int length;
 
-    /* Vérifie si str est NULL */
-    if (str == NULL)
-        return NULL;
+if (str == NULL)
+return (NULL);
 
-    /* Calcule la longueur de la chaîne d'origine */
-    len = strlen(str);
+length = strlen(str) + 1;
+duplicate = malloc(length *sizeof(char));
 
-    /* Alloue de la mémoire pour la copie (taille + 1 pour '\0') */
-    copy = (char *)malloc((len + 1) * sizeof(char));
-    if (copy == NULL) /* Vérifie si malloc a échoué */
-        return NULL;
+if (duplicate == NULL)
+return (NULL);
 
-    /* Copie chaque caractère de str dans copy */
-    for (i = 0; i <= len; i++) /* Inclut le caractère nul '\0' */
-        copy[i] = str[i];
-
-    return copy;
-}
-
+strcpy(duplicate, str);
+return (duplicate); }
